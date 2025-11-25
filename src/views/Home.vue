6901f9cb0b38ce3766921f57f8dc32320a6e12c1
diakauankl <kauanluciano673@gmail.com>
2025-11-25 10:36:52 -0300

    FEAT-3: Terminado a tela de boas vindas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,17 +13,45 @@
     Seja bem vindo ao StreamPlatform!
   </h1>
   <p>
-    Aqui você encontrará uma grande variedade de filmes, além de saber em qual plataforma de streaming cada um está disponível. Aproveite e descubra seu próximo filme favorito!
+    Aqui você encontrará uma grande variedade de <br> filmes, além de saber em qual plataforma de <br> streaming cada um está disponível.  <br> Aproveite e descubra seu próximo filme favorito!
   </p>
 </div>
 <div class="imagem">
- <img src="" alt="">
+ <img src="/public/img/boas-vindas.png" alt="boas/vindas" width="480vw" height="480vw">
 
 </div>
 
   </section>
+
+  <hr>
+
 </template>
 
 <style scoped>
+section {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
+}
+.texto {
+  text-align: center;
+  margin: 4vw 0 0 15vw;
+
+}
+.texto h1 {
+  font-size: 2.2vw;
+}
+.texto p {
+  font-size: 1.3vw;
+  margin-top: 1vw;
+}
+.imagem {
+  margin: 4vw 15vw 0 0;
+}
+hr {
+  margin: 6vw 3vw 0 3vw;
+   border: 4.3px solid #1D1D1D;
+
+}
 </style>

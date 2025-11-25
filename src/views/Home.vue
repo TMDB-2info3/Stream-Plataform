9c82f2b948e6380e2b222a4cd1645dcee700c7e1
diakauankl <kauanluciano673@gmail.com>
2025-11-25 10:06:48 -0300

    FEAT3 criada

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,29 @@
+<script setup>
+
+</script>
+
 <template>
   <div>
     <h1>Home</h1>
   </div>
+
+  <section>
+<div class="texto">
+  <h1>
+    Seja bem vindo ao StreamPlatform!
+  </h1>
+  <p>
+    Aqui você encontrará uma grande variedade de filmes, além de saber em qual plataforma de streaming cada um está disponível. Aproveite e descubra seu próximo filme favorito!
+  </p>
+</div>
+<div class="imagem">
+ <img src="" alt="">
+
+</div>
+
+  </section>
 </template>
+
+<style scoped>
+
+</style>
